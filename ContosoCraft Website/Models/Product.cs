b98e467a41ee170777c9302b58d3d670fef47f7e
ThreshLovesAhri <46910779+ThreshLovesAhri@users.noreply.github.com>
2020-01-10 13:03:14 -0600

    Add files via upload

---
+++ ContosoCraft Website/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ContosoCraft_Website.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")] //Mapping the image of the jason to Image in this class
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this); //Taking all the object information converting all of it back to the json as a simple text
+        
+    }
+}
---
+++ ContosoCraft Website/Services/JsonFileProductService.cs
@@ -0,0 +1,74 @@
+﻿using ContosoCraft_Website.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ContosoCraft_Website.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment) 
+            //Host Enviroment Information, Create a Host, He will know where things are at
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+            //Ths Path Combine makes that the webhost loock out to the folder and the name of the file 
+            //No matter what path it has it would find it
+            }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+                    //IEnumerable makes the list, it make it to a list
+                //This deserializer convert the text to the actual product, taking json to object
+            }
+        }
+        //Retreaving the products 
+
+        public void AddRating(string productId, int rating)
+        {
+            var products = GetProducts();
+
+            if (products.First(x => x.Id == productId).Ratings == null)
+            {
+                products.First(x => x.Id == productId).Ratings = new int[] { rating };
+            }
+            else
+            {
+                var ratings = products.First(x => x.Id == productId).Ratings.ToList();
+                ratings.Add(rating);
+                products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+            }
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+
+        }
+    }
+}
